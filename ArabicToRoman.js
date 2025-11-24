efb479b17de34949b70a55cf70424a74cd23001e
Lautaro Gutierrez Lardit <lau.gutlar55@gmail.com>
2025-11-24 15:24:37 -0300

    Commit9

--- ArabicToRoman.js
+++ ArabicToRoman.js
@@ -31,3 +31,4 @@ function convertirARomano(numero) {
     
     return resultado;
 }
+module.exports = { ArabicToRoman };
--- RomanToArabic.js
+++ RomanToArabic.js
@@ -83,3 +83,4 @@ class RomanConverter {
     }
 
 }
+module.exports = {RomanToArabic};
